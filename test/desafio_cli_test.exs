a53143c789802ec107d03213924d9039ed41a3cc
Lucas Henrique <lucasmula125@gmail.com>
2024-07-18 03:34:36 +0000

    massive unique names test

--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -6,48 +6,65 @@ defmodule DesafioCliTest do
   test "More than 4 thousand equal names" do
     {result, _ok_names, failed_names} = enumerate(NameGen.repeat_name("Lucas", 5000))
     assert result == :err
-      && Enum.filter(failed_names, fn name -> name != "Lucas" end) == []
+    && Enum.filter(failed_names, fn name -> name != "Lucas" end) == []
   end
 
   test "Needs to subtract" do
     {result, ok_names, failed_names} = enumerate(NameGen.repeat_name("Akita", 4))
     assert result == :ok
-      && List.last(ok_names) == "Akita IV"
-      && failed_names == []
+    && List.last(ok_names) == "Akita IV"
+    && failed_names == []
   end
 
   test "Adding up to 10" do
     {result, ok_names, failed_names} = enumerate(NameGen.repeat_name("Paulo", 10))
     assert result == :ok
-      && ok_names == ["Paulo I", "Paulo II", "Paulo III", "Paulo IV", "Paulo V", "Paulo VI", "Paulo VII", "Paulo VIII", "Paulo IX", "Paulo X"]
-      && failed_names == []
+    && ok_names == ["Paulo I", "Paulo II", "Paulo III", "Paulo IV", "Paulo V", "Paulo VI", "Paulo VII", "Paulo VIII", "Paulo IX", "Paulo X"]
+    && failed_names == []
   end
 
   test "Needs to add up three times" do
     {result, ok_names, failed_names} = enumerate(NameGen.repeat_name("Cumbuca", 80))
     assert result == :ok
-      && List.last(ok_names) == "Cumbuca LXXX"
-      && failed_names == []
+    && List.last(ok_names) == "Cumbuca LXXX"
+    && failed_names == []
   end
 
   test "Only unique names" do
     names = ["Lucas", "Luiz", "Gercino", "Dayse", "David", "Wictor", "Aline"]
     {result, ok_names, failed_names} = enumerate(names)
     assert result == :ok
-      && ok_names == Enum.map(names, fn name -> name <> " I" end)
-      && failed_names == []
+    && ok_names == Enum.map(names, fn name -> name <> " I" end)
+    && failed_names == []
   end
 
   test "No names given" do
     {result, ok_names, failed_names} = enumerate([])
     assert result == :ok
-      && ok_names == []
-      && failed_names == []
+    && ok_names == []
+    && failed_names == []
+  end
+
+  @tag timeout: :infinity
+  test "A shit ton of unique names" do
+    IO.puts("Generating names...")
+    names = NameGen.gen_names(100_000, [])
+    {result, ok_names, failed_names} = enumerate(names)
+    assert result == :ok
+    && ok_names == Enum.map(names, fn name -> name <> " I" end)
+    && failed_names == []
   end
 end
 
 defmodule NameGen do
   def repeat_name(name, amount) do
     List.duplicate(name, amount)
+  end
+  def gen_names(amount, list) when amount > 0 do
+    list = list ++ ["#{amount}"]
+    gen_names(amount - 1, list)
+  end
+  def gen_names(0, list) do
+    list
   end
 end
